5a9cb667f2cf2647f033f23da10cf3954bfcd917
Cem KOK <cemkok@outlook.com>
2022-04-07 19:10:23 +0300

    Fizz-Buzz

---
+++ src/main/java/io/cem/course/unittest/unittestnedir/FizzBuzz.java
@@ -0,0 +1,16 @@
+package io.cem.course.unittest.unittestnedir;
+
+public class FizzBuzz {
+
+    public String stringFor(int number) {
+
+        if ((number % 3) == 0) {
+            return "Fizz";
+
+        }
+        if (number%5==0) {
+            return "Buzz";
+        }
+        return null;
+    }
+}
---
+++ src/test/java/io/cem/course/unittest/unittestnedir/FizzBuzzTest.java
@@ -0,0 +1,30 @@
+package io.cem.course.unittest.unittestnedir;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void returnFizzWhenTheNumberIsDividedByThree(){
+
+        assertEquals("Fizz", fizzBuzz.stringFor(3));
+
+    }
+
+    @Test
+    void returnBuzzWhenTheNumberIsDividedByFive(){
+        assertEquals("Buzz", fizzBuzz.stringFor(5));
+
+    }
+}
