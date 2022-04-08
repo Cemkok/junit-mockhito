9b9cd8fd32a52e290afcb6a6ee23c20b19f93109
Cem KOK <cemkok@outlook.com>
2022-04-08 09:50:45 +0300

    Fizz-Buzz

--- src/main/java/io/cem/course/unittest/unittestnedir/FizzBuzz.java
+++ src/main/java/io/cem/course/unittest/unittestnedir/FizzBuzz.java
@@ -4,13 +4,22 @@ public class FizzBuzz {
 
     public String stringFor(int number) {
 
-        if ((number % 3) == 0) {
+        if(number<1 || number>100){
+            throw new IllegalArgumentException();
+        }
+
+        if (number % 15 == 0) {
+
+            return "FizzBuzz";
+        }
+
+         if ((number % 3) == 0) {
             return "Fizz";
 
-        }
-        if (number%5==0) {
+          }
+         if (number%5==0) {
             return "Buzz";
-        }
-        return null;
+         }
+         return String.valueOf(number);
     }
 }
--- src/test/java/io/cem/course/unittest/unittestnedir/FizzBuzzTest.java
+++ src/test/java/io/cem/course/unittest/unittestnedir/FizzBuzzTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FizzBuzzTest {
 
@@ -27,4 +28,25 @@ public class FizzBuzzTest {
         assertEquals("Buzz", fizzBuzz.stringFor(5));
 
     }
-}
+
+    @Test
+    void returnFizzBuzzWhenTheNumberIsDividedBothOfThreeAndFive(){
+        assertEquals("FizzBuzz", fizzBuzz.stringFor(15));
+    }
+
+    @Test
+    void returnTheNumberItselfWhenTheNumberIsNotDividedAnyOfThreeOrFive(){
+        assertEquals("7",fizzBuzz.stringFor(7));
+
+    }
+    @Test
+    void throwsIllegalArgumentExceptionWhenTheNumberIsLessThanOneOrGreaterThanHundred(){
+        assertThrows(IllegalArgumentException.class, ()->fizzBuzz.stringFor(-1));
+        assertThrows(IllegalArgumentException.class, ()->fizzBuzz.stringFor(101));
+    }
+
+
+    }
+
+
+
